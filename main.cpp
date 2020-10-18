c6664a87165352088f9951cb24226c09866b2f80
J-B <59450754+JB-toriel@users.noreply.github.com>
2020-10-18 19:37:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/*
+
+██████╗ ██╗      █████╗ ████████╗███████╗ ██████╗ ██████╗ ███╗   ███╗    ███████╗██╗███╗   ███╗██╗   ██╗██╗      █████╗ ████████╗ ██████╗ ██████╗ 
+██╔══██╗██║     ██╔══██╗╚══██╔══╝██╔════╝██╔═══██╗██╔══██╗████╗ ████║    ██╔════╝██║████╗ ████║██║   ██║██║     ██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+██████╔╝██║     ███████║   ██║   █████╗  ██║   ██║██████╔╝██╔████╔██║    ███████╗██║██╔████╔██║██║   ██║██║     ███████║   ██║   ██║   ██║██████╔╝
+██╔═══╝ ██║     ██╔══██║   ██║   ██╔══╝  ██║   ██║██╔══██╗██║╚██╔╝██║    ╚════██║██║██║╚██╔╝██║██║   ██║██║     ██╔══██║   ██║   ██║   ██║██╔══██╗
+██║     ███████╗██║  ██║   ██║   ██║     ╚██████╔╝██║  ██║██║ ╚═╝ ██║    ███████║██║██║ ╚═╝ ██║╚██████╔╝███████╗██║  ██║   ██║   ╚██████╔╝██║  ██║
+╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝    ╚══════╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+                                                                                                                                                  
+*/
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -13,19 +24,65 @@
 
 using namespace std;
 
+string getFileContents (ifstream& File)
+{
+    string Lines = "";        //All lines
+    
+    if (File)                      //Check if everything is good
+    {
+	while (File.good ())
+	{
+	    string TempLine;                  //Temp line
+	    getline (File , TempLine);        //Get temp line
+	    TempLine += "\n";                      //Add newline character
+	    
+	    Lines += TempLine;                     //Add newline
+	}
+	return Lines;
+    }
+    else                           //Return error
+			return "ERROR File does not exist.";
+}
 
 
 int main(int argc, char* argv[]){
 
+	/* ---------Logo---------*/
+
+		ifstream Reader ("Logo.txt");             //Open file
+
+    string Art = getFileContents (Reader);       //Get file
+    
+    cout << Art << endl;               //Print it to the screen
+
+    Reader.close ();                           //Close file
+
+	/*--------END Logo-------*/
+
   Platform platform;
 
-  if( platform.load( argv[1]) )
+  if( argc<2 )
+    return EXIT_FAILURE;
+  
+  if(  platform.load( argv[1] ) == EXIT_FAILURE)
     return EXIT_FAILURE;
 
   platform.bind();
 
   platform.simulate();
 
+	/* ---------Logo---------*/
+
+		Reader.open("end.txt");             //Open file
+
+    Art = getFileContents (Reader);       //Get file
+    
+    cout << Art << endl;               //Print it to the screen
+
+    Reader.close ();                           //Close file
+
+	/*--------END Logo-------*/
+	
   cout << "//////////////////////THE END/////////////////////////" << endl;
 
   return EXIT_SUCCESS;
